4a30b842185f1a055636363a9c6cc7f029b5511b
thirstydeveloper <thirstydeveloper@gmail.com>
2020-12-05 06:37:34 -0500

    feat: add variables to test-stack

--- modules/stacks/app/test-stack/outputs.tf
+++ modules/stacks/app/test-stack/outputs.tf
@@ -1,3 +1,23 @@
 output "pet" {
   value = random_pet.pet.id
 }
+
+output "global_var" {
+  value = var.global_var
+}
+
+output "tier_var" {
+  value = var.tier_var
+}
+
+output "env_var" {
+  value = var.env_var
+}
+
+output "layer_var" {
+  value = var.layer_var
+}
+
+output "stack_var" {
+  value = var.stack_var
+}
---
+++ modules/stacks/app/test-stack/variables.tf
@@ -0,0 +1,19 @@
+variable "global_var" {
+  default = "unset"
+}
+
+variable "tier_var" {
+  default = "unset"
+}
+
+variable "env_var" {
+  default = "unset"
+}
+
+variable "layer_var" {
+  default = "unset"
+}
+
+variable "stack_var" {
+  default = "unset"
+}
